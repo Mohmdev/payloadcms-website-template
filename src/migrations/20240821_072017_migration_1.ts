9c9c04fec4bc9e0370f7a168a0187a459cac5017
Mohamad <Mohammadr_m@outlook.com>
2024-08-21 11:27:04 +0300

    Chore: Refactor deployment configuration to use npm

---
+++ src/migrations/20240821_072017_migration_1.ts
@@ -0,0 +1,13 @@
+import {
+  MigrateUpArgs,
+  MigrateDownArgs,
+} from '@payloadcms/db-mongodb'
+undefined
+
+export async function up({ payload }: MigrateUpArgs): Promise<void> {
+  // Migration code
+}
+
+export async function down({ payload }: MigrateDownArgs): Promise<void> {
+  // Migration code
+}
