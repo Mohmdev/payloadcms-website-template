171c61a8e5eeb0bed2873ebb7daf8c95277d0130
Mohamad <Mohammadr_m@outlook.com>
2024-11-20 13:43:19 +0000

    Chore: Update project with the latest production ready payload 3.0.1

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
-import * as migration_20240831_094937_initial from './20240831_094937_initial';
-import * as migration_20241014_085848_update_v3_0_0_beta_113 from './20241014_085848_update_v3_0_0_beta_113';
+import * as migration_20240831_094937_initial from './20240831_094937_initial'
+import * as migration_20241014_085848_update_v3_0_0_beta_113 from './20241014_085848_update_v3_0_0_beta_113'
+import * as migration_20241120_133552_payload_v3_production from './20241120_133552_payload_v3_production'
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20241014_085848_update_v3_0_0_beta_113.up,
     down: migration_20241014_085848_update_v3_0_0_beta_113.down,
-    name: '20241014_085848_update_v3_0_0_beta_113'
+    name: '20241014_085848_update_v3_0_0_beta_113',
   },
-];
+  {
+    up: migration_20241120_133552_payload_v3_production.up,
+    down: migration_20241120_133552_payload_v3_production.down,
+    name: '20241120_133552_payload_v3_production',
+  },
+]
---
+++ src/plugins/index.ts
@@ -0,0 +1,114 @@
+import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
+import { formBuilderPlugin } from '@payloadcms/plugin-form-builder'
+import { nestedDocsPlugin } from '@payloadcms/plugin-nested-docs'
+import { redirectsPlugin } from '@payloadcms/plugin-redirects'
+import { seoPlugin } from '@payloadcms/plugin-seo'
+import { searchPlugin } from '@payloadcms/plugin-search'
+import { Plugin } from 'payload'
+import { revalidateRedirects } from '@/hooks/revalidateRedirects'
+import { GenerateTitle, GenerateURL } from '@payloadcms/plugin-seo/types'
+import { FixedToolbarFeature, HeadingFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
+import { searchFields } from '@/search/fieldOverrides'
+import { beforeSyncWithSearch } from '@/search/beforeSync'
+import { s3Storage } from '@payloadcms/storage-s3'
+// import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
+
+import { Page, Post } from '@/payload-types'
+import { getServerSideURL } from '@/utilities/getURL'
+
+const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
+  return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
+}
+
+const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
+  const url = getServerSideURL()
+
+  return doc?.slug ? `${url}/${doc.slug}` : url
+}
+
+export const plugins: Plugin[] = [
+  s3Storage({
+    collections: {
+      media: {
+        prefix: 'media',
+      },
+    },
+    disableLocalStorage: true,
+    acl: 'private',
+    bucket: process.env.S3_BUCKET as string,
+    config: {
+      endpoint: process.env.S3_ENDPOINT,
+      forcePathStyle: true,
+      region: 'us-east-1', // Dummy region to avoid error
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY as string,
+        secretAccessKey: process.env.S3_SECRET_KEY as string,
+      },
+    },
+  }),
+  redirectsPlugin({
+    collections: ['pages', 'posts'],
+    overrides: {
+      // @ts-expect-error
+      fields: ({ defaultFields }) => {
+        return defaultFields.map((field) => {
+          if ('name' in field && field.name === 'from') {
+            return {
+              ...field,
+              admin: {
+                description: 'You will need to rebuild the website when changing this field.',
+              },
+            }
+          }
+          return field
+        })
+      },
+      hooks: {
+        afterChange: [revalidateRedirects],
+      },
+    },
+  }),
+  nestedDocsPlugin({
+    collections: ['categories'],
+  }),
+  seoPlugin({
+    generateTitle,
+    generateURL,
+  }),
+  formBuilderPlugin({
+    fields: {
+      payment: false,
+    },
+    formOverrides: {
+      fields: ({ defaultFields }) => {
+        return defaultFields.map((field) => {
+          if ('name' in field && field.name === 'confirmationMessage') {
+            return {
+              ...field,
+              editor: lexicalEditor({
+                features: ({ rootFeatures }) => {
+                  return [
+                    ...rootFeatures,
+                    FixedToolbarFeature(),
+                    HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4'] }),
+                  ]
+                },
+              }),
+            }
+          }
+          return field
+        })
+      },
+    },
+  }),
+  searchPlugin({
+    collections: ['posts'],
+    beforeSync: beforeSyncWithSearch,
+    searchOverrides: {
+      fields: ({ defaultFields }) => {
+        return [...defaultFields, ...searchFields]
+      },
+    },
+  }),
+  payloadCloudPlugin(),
+]
