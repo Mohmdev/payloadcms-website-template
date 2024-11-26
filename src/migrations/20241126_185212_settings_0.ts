23a37bbe9b9a93333c8c03cdc70f251843b5049b
Mohmdev <Mohammadr_m@outlook.com>
2024-11-26 21:59:16 +0300

    Chore: Update media loading strategy to use lazy loading and disable priority; rename and remove obsolete migration files

--- src/migrations/20241126_185212_settings_0.ts
+++ src/migrations/20241126_185212_settings_0.ts
@@ -6,6 +6,7 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   	"id" serial PRIMARY KEY NOT NULL,
   	"alt" varchar,
   	"caption" varchar,
+  	"prefix" varchar DEFAULT 'assets',
   	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
   	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
   	"url" varchar,
@@ -55,23 +56,40 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   	"sizes_xlarge_filename" varchar
   );
   
-  ALTER TABLE "graphics" DROP CONSTRAINT "graphics_site_logo_id_graphics_id_fk";
+  CREATE TABLE IF NOT EXISTS "site_info" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"site_name" varchar,
+  	"site_description" varchar,
+  	"updated_at" timestamp(3) with time zone,
+  	"created_at" timestamp(3) with time zone
+  );
   
-  ALTER TABLE "graphics" DROP CONSTRAINT "graphics_favicon_id_graphics_id_fk";
+  CREATE TABLE IF NOT EXISTS "graphics" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"site_logo_id" integer,
+  	"favicon_id" integer,
+  	"updated_at" timestamp(3) with time zone,
+  	"created_at" timestamp(3) with time zone
+  );
   
-  ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_graphics_fk";
+  CREATE TABLE IF NOT EXISTS "contact_information" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"contact_name" varchar,
+  	"contact_email" varchar,
+  	"contact_phone" varchar,
+  	"contact_address" varchar,
+  	"social_media_facebook" varchar,
+  	"social_media_twitter" varchar,
+  	"social_media_instagram" varchar,
+  	"social_media_linkedin" varchar,
+  	"social_media_youtube" varchar,
+  	"social_media_whatsapp" varchar,
+  	"social_media_telegram" varchar,
+  	"updated_at" timestamp(3) with time zone,
+  	"created_at" timestamp(3) with time zone
+  );
   
-  DROP INDEX IF EXISTS "payload_locked_documents_rels_graphics_id_idx";
   ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "assets_id" integer;
-  CREATE INDEX IF NOT EXISTS "assets_updated_at_idx" ON "assets" USING btree ("updated_at");
-  CREATE INDEX IF NOT EXISTS "assets_created_at_idx" ON "assets" USING btree ("created_at");
-  CREATE UNIQUE INDEX IF NOT EXISTS "assets_filename_idx" ON "assets" USING btree ("filename");
-  CREATE INDEX IF NOT EXISTS "assets_sizes_thumbnail_sizes_thumbnail_filename_idx" ON "assets" USING btree ("sizes_thumbnail_filename");
-  CREATE INDEX IF NOT EXISTS "assets_sizes_square_sizes_square_filename_idx" ON "assets" USING btree ("sizes_square_filename");
-  CREATE INDEX IF NOT EXISTS "assets_sizes_small_sizes_small_filename_idx" ON "assets" USING btree ("sizes_small_filename");
-  CREATE INDEX IF NOT EXISTS "assets_sizes_medium_sizes_medium_filename_idx" ON "assets" USING btree ("sizes_medium_filename");
-  CREATE INDEX IF NOT EXISTS "assets_sizes_large_sizes_large_filename_idx" ON "assets" USING btree ("sizes_large_filename");
-  CREATE INDEX IF NOT EXISTS "assets_sizes_xlarge_sizes_xlarge_filename_idx" ON "assets" USING btree ("sizes_xlarge_filename");
   DO $$ BEGIN
    ALTER TABLE "graphics" ADD CONSTRAINT "graphics_site_logo_id_assets_id_fk" FOREIGN KEY ("site_logo_id") REFERENCES "public"."assets"("id") ON DELETE set null ON UPDATE no action;
   EXCEPTION
@@ -84,48 +102,38 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
    WHEN duplicate_object THEN null;
   END $$;
   
+  CREATE INDEX IF NOT EXISTS "assets_updated_at_idx" ON "assets" USING btree ("updated_at");
+  CREATE INDEX IF NOT EXISTS "assets_created_at_idx" ON "assets" USING btree ("created_at");
+  CREATE UNIQUE INDEX IF NOT EXISTS "assets_filename_idx" ON "assets" USING btree ("filename");
+  CREATE INDEX IF NOT EXISTS "assets_sizes_thumbnail_sizes_thumbnail_filename_idx" ON "assets" USING btree ("sizes_thumbnail_filename");
+  CREATE INDEX IF NOT EXISTS "assets_sizes_square_sizes_square_filename_idx" ON "assets" USING btree ("sizes_square_filename");
+  CREATE INDEX IF NOT EXISTS "assets_sizes_small_sizes_small_filename_idx" ON "assets" USING btree ("sizes_small_filename");
+  CREATE INDEX IF NOT EXISTS "assets_sizes_medium_sizes_medium_filename_idx" ON "assets" USING btree ("sizes_medium_filename");
+  CREATE INDEX IF NOT EXISTS "assets_sizes_large_sizes_large_filename_idx" ON "assets" USING btree ("sizes_large_filename");
+  CREATE INDEX IF NOT EXISTS "assets_sizes_xlarge_sizes_xlarge_filename_idx" ON "assets" USING btree ("sizes_xlarge_filename");
+  CREATE INDEX IF NOT EXISTS "graphics_site_logo_idx" ON "graphics" USING btree ("site_logo_id");
+  CREATE INDEX IF NOT EXISTS "graphics_favicon_idx" ON "graphics" USING btree ("favicon_id");
   DO $$ BEGIN
    ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_assets_fk" FOREIGN KEY ("assets_id") REFERENCES "public"."assets"("id") ON DELETE cascade ON UPDATE no action;
   EXCEPTION
    WHEN duplicate_object THEN null;
   END $$;
   
-  CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_assets_id_idx" ON "payload_locked_documents_rels" USING btree ("assets_id");
-  ALTER TABLE "media" DROP COLUMN IF EXISTS "prefix";
-  ALTER TABLE "payload_locked_documents_rels" DROP COLUMN IF EXISTS "graphics_id";`)
+  CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_assets_id_idx" ON "payload_locked_documents_rels" USING btree ("assets_id");`)
 }
 
 export async function down({ payload, req }: MigrateDownArgs): Promise<void> {
   await payload.db.drizzle.execute(sql`
    ALTER TABLE "assets" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "site_info" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "graphics" DISABLE ROW LEVEL SECURITY;
+  ALTER TABLE "contact_information" DISABLE ROW LEVEL SECURITY;
   DROP TABLE "assets" CASCADE;
+  DROP TABLE "site_info" CASCADE;
+  DROP TABLE "graphics" CASCADE;
+  DROP TABLE "contact_information" CASCADE;
   ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_assets_fk";
   
-  ALTER TABLE "graphics" DROP CONSTRAINT "graphics_site_logo_id_assets_id_fk";
-  
-  ALTER TABLE "graphics" DROP CONSTRAINT "graphics_favicon_id_assets_id_fk";
-  
   DROP INDEX IF EXISTS "payload_locked_documents_rels_assets_id_idx";
-  ALTER TABLE "media" ADD COLUMN "prefix" varchar DEFAULT 'media';
-  ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "graphics_id" integer;
-  DO $$ BEGIN
-   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_graphics_fk" FOREIGN KEY ("graphics_id") REFERENCES "public"."graphics"("id") ON DELETE cascade ON UPDATE no action;
-  EXCEPTION
-   WHEN duplicate_object THEN null;
-  END $$;
-  
-  DO $$ BEGIN
-   ALTER TABLE "graphics" ADD CONSTRAINT "graphics_site_logo_id_graphics_id_fk" FOREIGN KEY ("site_logo_id") REFERENCES "public"."graphics"("id") ON DELETE set null ON UPDATE no action;
-  EXCEPTION
-   WHEN duplicate_object THEN null;
-  END $$;
-  
-  DO $$ BEGIN
-   ALTER TABLE "graphics" ADD CONSTRAINT "graphics_favicon_id_graphics_id_fk" FOREIGN KEY ("favicon_id") REFERENCES "public"."graphics"("id") ON DELETE set null ON UPDATE no action;
-  EXCEPTION
-   WHEN duplicate_object THEN null;
-  END $$;
-  
-  CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_graphics_id_idx" ON "payload_locked_documents_rels" USING btree ("graphics_id");
   ALTER TABLE "payload_locked_documents_rels" DROP COLUMN IF EXISTS "assets_id";`)
 }
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,10 +2,7 @@ import * as migration_20240831_094937_initial from './20240831_094937_initial';
 import * as migration_20241014_085848_update_v3_0_0_beta_113 from './20241014_085848_update_v3_0_0_beta_113';
 import * as migration_20241120_133552_payload_v3_production from './20241120_133552_payload_v3_production';
 import * as migration_20241122_141949_update_v3_0_2_production from './20241122_141949_update_v3_0_2_production';
-import * as migration_20241125_144857_settings_0 from './20241125_144857_settings_0';
-import * as migration_20241125_200556_settings_1 from './20241125_200556_settings_1';
-import * as migration_20241125_222935_settings_2 from './20241125_222935_settings_2';
-import * as migration_20241125_223248_settings_3 from './20241125_223248_settings_3';
+import * as migration_20241126_185212_settings_0 from './20241126_185212_settings_0';
 
 export const migrations = [
   {
@@ -29,23 +26,8 @@ export const migrations = [
     name: '20241122_141949_update_v3_0_2_production',
   },
   {
-    up: migration_20241125_144857_settings_0.up,
-    down: migration_20241125_144857_settings_0.down,
-    name: '20241125_144857_settings_0',
-  },
-  {
-    up: migration_20241125_200556_settings_1.up,
-    down: migration_20241125_200556_settings_1.down,
-    name: '20241125_200556_settings_1',
-  },
-  {
-    up: migration_20241125_222935_settings_2.up,
-    down: migration_20241125_222935_settings_2.down,
-    name: '20241125_222935_settings_2',
-  },
-  {
-    up: migration_20241125_223248_settings_3.up,
-    down: migration_20241125_223248_settings_3.down,
-    name: '20241125_223248_settings_3'
+    up: migration_20241126_185212_settings_0.up,
+    down: migration_20241126_185212_settings_0.down,
+    name: '20241126_185212_settings_0'
   },
 ];
